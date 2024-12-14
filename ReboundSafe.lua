507971b4f622aff420dfdac92abcb59f4355c73c
Tupo <denduhadelaetstrimy@gmail.com>
2024-12-14 17:20:49 +0300

    Create ReboundSafe.lua

---
+++ ReboundSafe.lua
@@ -0,0 +1,54 @@
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
+local speed = math.random(350,1050)
+local entity = spawner.Create({
+	Entity = {
+		Name = "rebooundMoving",
+		Asset = "https://github.com/Tupoi-Scripts/Entity-Spawner/blob/main/RushMoving.rbxm?raw=true",
+		HeightOffset = 1
+	},
+	Lights = {
+		Flicker = {
+			Enabled = true,
+			Duration = 5
+		},
+		Shatter = true,
+		Repair = false
+	},
+	Earthquake = {
+		Enabled = false
+	},
+	CameraShake = {
+		Enabled = true,
+		Range = 100,
+		Values = {5, 15, 0.1, 1}
+	},
+	Movement = {
+		Speed = speed,
+		Delay = 4,
+		Reversed = true
+	},
+	Rebounding = {
+		Enabled = true,
+		Type = "Blitz",
+		Min = 2,
+		Max = 5,
+		Delay = 1.5
+	},
+	Damage = {
+		Enabled = false,
+		Range = 40,
+		Amount = 175
+	},
+	Crucifixion = {
+		Enabled = true,
+		Range = 40,
+		Resist = true,
+		Break = false
+	},
+	Death = {
+		Type = "Guiding",
+		Hints = {"Sorry", ":Troll:", "lol", "try again"},
+		Cause = "Rebound"
+	}
+})
+entity:Run()
